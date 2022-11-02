fd76761a2feadb3bd68cd0cd6d7aaf1219cad51b
yevgeller@gmail.com <yevgeller@gmail.com>
2022-11-01 22:14:34 -0400

    Interim commit, working on components

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,10 +1,15 @@
 export default {
-    template: `<button class='bg-gray-800 hover:bg-gray-400 border rounded px-5 py-2' 
+    template: `<button :class="{'border rounded px-5 py-2' :true,
+    'bg-blue-800 hover:bg-blue-400 ' : type === 'primary',
+    'bg-purple-800 hover:bg-purple-400 ' : type === 'secondary',
+    'bg-gray-800 hover:bg-gray-400 ' : type === 'muted',
+    'is-loading' : processing
+}"
                     :disabled="processing">
                         <slot /></button>`
-    , data() {
-        return {
-            processing: false
-        }
-    }
+    ,
+    props: {
+        type: { type: String, default: 'primary' },
+        processing: { type: Boolean, default: false },
+    },
 }
